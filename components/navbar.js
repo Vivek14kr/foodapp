0e1e1afbd05f787a79935bb1cf28b26fffaeb640
vivek <vivekkumarvky13@gmail.com>
2021-10-20 19:15:43 +0530

    foodapp

---
+++ components/navbar.js
@@ -0,0 +1,13 @@
+function navbar() {
+    return `
+
+        <h3><a href="food.html">Home</a></h3>
+        <div id="options">
+            <h3><a href="recofday.html">Receipe of the day</a></h3>
+            <h3><a href="latest.html">Latest Recipe</a></h3>
+    
+        </div>
+    `
+}
+
+export default navbar
